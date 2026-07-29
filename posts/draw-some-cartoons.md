8b60928bd9d0e2acacd367ea57bd61ab24799c13
Sam Lowe <samdassi7@me.com>
2026-07-29 18:25:22 -0400

    new post

--- posts/draw-some-cartoons.md
+++
@@ -1,7 +0,0 @@
----
-title: "29 Before 29 #7: Draw Some Cartoons"
-subtitle: 
-summary: 
-publishedAt: "2026-07-28"
----
-I told you I’d be back soon, didn’t I? No rest for the weary while there are still items to be crossed out on the list. 
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -91,6 +91,43 @@ function App() {
 				<div className="text-[#ECE6E6] space-y-3 mt-2">
 					<div className="border-l-2 border-[#77FFE4] pl-3">
 						<p>
+							<span className="text-[#ECE6E6]">07/28/2026</span> - Subjecting the internet
+							to my{" "}
+							<Link
+								to="/posts/$slug"
+								params={{ slug: "draw-some-cartoons" }}
+								className="text-[#FFE72C]"
+							>
+								stick figures
+							</Link>
+							<img
+								src="/images/link.png"
+								alt=""
+								className="inline w-3 h-3 ml-[2px] mb-[2px]"
+							/>
+							.
+						</p>
+					</div>
+					<div className="border-l-2 border-[#77FFE4] pl-3">
+						<p>
+							<span className="text-[#ECE6E6]">07/28/2026</span> - Games aren't{" "}
+							<Link
+								to="/posts/$slug"
+								params={{ slug: "develop-a-game" }}
+								className="text-[#FFE72C]"
+							>
+								just for computers
+							</Link>
+							<img
+								src="/images/link.png"
+								alt=""
+								className="inline w-3 h-3 ml-[2px] mb-[2px]"
+							/>
+							.
+						</p>
+					</div>
+					<div className="border-l-2 border-[#77FFE4] pl-3">
+						<p>
 							<span className="text-[#ECE6E6]">07/15/2026</span> - My internet
 							kingdom{" "}
 							<Link
