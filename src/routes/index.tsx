a69b7d13f7049347ad41730ca7363670006e39dc
Sam Lowe <samdassi7@me.com>
2026-09-11 18:05:30 -0400

    post

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -91,6 +91,25 @@ function App() {
 				<div className="text-[#ECE6E6] space-y-3 mt-2">
 					<div className="border-l-2 border-[#77FFE4] pl-3">
 						<p>
+							<span className="text-[#ECE6E6]">09/11/2026</span> - A case
+							for{" "}
+							<Link
+								to="/posts/$slug"
+								params={{ slug: "embrace-the-incremental" }}
+								className="text-[#FFE72C]"
+							>
+								the gradual
+							</Link>
+							<img
+								src="/images/link.png"
+								alt=""
+								className="inline w-3 h-3 ml-[2px] mb-[2px]"
+							/>
+							.
+						</p>
+					</div>
+					<div className="border-l-2 border-[#77FFE4] pl-3">
+						<p>
 							<span className="text-[#ECE6E6]">09/10/2026</span> - Starting
 							the{" "}
 							<Link
